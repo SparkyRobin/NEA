f6ae12c9269edadefe765626d1ab87013ac6d46e
SparkyRobin <srparker2020@gmail.com>
2021-10-25 04:53:54 +0100

    2021/10/25 03:53
    Needs work on model - It does not generalise.
    Possibly would benefit from transitioning from yfinance to a different service.
    Using PyQT6. Need to build sign in page.

--- Frontend.py
+++ Frontend.py
@@ -1,25 +1,43 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import Qt
+import inspect
 
 class startup(QWidget):
 
     def __init__(self):
         super().__init__()
 
-        self.resize(1500, 1000)
+        self.resize(1000, 1000)
         self.centre()
+        self.vlay = QVBoxLayout(self)
+        #self.vlay.addItem(QSpacerItem(0, 0))
+        #self.vlay.addStretch(1)
+        self.button("Sam", y=300)
+        self.button("Parker")
+        #self.vlay.addStretch(1)
+        #self.vlay.addItem(QSpacerItem(0, 0))
+        self.hlay = QHBoxLayout(self)
+        self.hlay.addStretch(1)
+        self.hlay.addLayout(self.vlay)
         self.setWindowTitle('Startup')
+        self.centre()
         self.show()
 
-        sys.exit(app.exec())
+    def button(self, name, x=0, y=0):
+        self.btn = QPushButton(name, self)
+        self.btn.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.vlay.addWidget(self.btn)
+
+
 
     def centre(self):
         qr = self.frameGeometry()
         cp = self.screen().availableGeometry().center()
-
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-
 app = QApplication(sys.argv)
 x = startup()
+sys.exit(app.exec())
