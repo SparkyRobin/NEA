99b4de61d012eb26fba0f91b6c150372a7b7af98
SparkyRobin <srparker2020@gmail.com>
2022-01-01 22:52:15 +0000

    2022/01/01 22:52
    Backup.

--- Backend.py
+++ Backend.py
@@ -5,6 +5,10 @@ import hashlib
 
 load_dotenv()
 
+
+def encryptKey(apiKey):
+
+     return(apiKey)
 def initCheck():
 
      hasAcc = os.environ.get('EXISTS')
@@ -18,25 +22,38 @@ def passCheck(password):
      user = os.environ.get('USER')
      salt = os.environ.get('SALT')
      print(salt)
-     salt = salt.encode('latin1')
+     salt = bytes.fromhex(salt)
      key = os.environ.get('HASH')
 
-     new_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-
+     new_key = str(hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000))
+     print()
 
      if new_key == key:
-          print('Password is correct')
+          return(True)
      else:
-          print('Password is incorrect')
+          return(False)
 
-def newAcc(password, apiKey):
+def newAcc(password, apiKey, walletName):
 
      salt = os.urandom(32)
      key = str(hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000))
-     salt = salt.decode('latin1')
-     print(salt)
+     salt = salt.hex()
+     apiKey = encryptKey(apiKey)
      with open('.env', 'r+') as env:
-          env.write(f'EXISTS=true\nHASH={key}\nSALT={str(salt)}\nKEY={str(apiKey)}') # Encrypt api key
+          env.write(f'EXISTS=true\nHASH={key}\nSALT={str(salt)}\nKEY={str(apiKey)}\nWALLETSNUM=1\nWALLETSNAME={walletName}\n') # Encrypt api key
+          env.close()
 
-newAcc("coolpassword", 1234)
-passCheck("coolpassword")
+def newWallet(apiKey, walletName):
+
+     apiKey = encryptKey(apiKey)
+     with open('.env', 'r+') as env:
+          aEnv = env.read().split('\n')
+          aEnv[3] = f'{aEnv[3]}:{apiKey}'
+          aEnv[4] = str(int(os.environ.get('WALLETSNUM'))+1)
+          aEnv[5] = f'{aEnv[5]}:{walletName}'
+          env.truncate()
+          for line in aEnv:
+               env.write(f'{line}\n')
+
+#newAcc('coolpassword', 1234, 'wallet0')
+newWallet(5678, 'wallet1')
